46ce8dbe91b3571848f09b333aa0e662c0d3db18
FarrukhMahkamov <farruhmahkamov20@gmail.com>
2023-09-22 14:21:24 +0500

    Add menu button texts

--- telegram/lang/lang.go
+++ telegram/lang/lang.go
@@ -1,1 +1,148 @@
 package lang
+
+const (
+	startComand = "start"
+	helpComand  = "help"
+	aboutComand = "about"
+)
+
+var (
+
+	// Main menu buttons
+	Income = map[string]string{
+		"uz":     "⬇️Kirim",
+		"ru":     "⬇️Доход",
+		"en":     "⬇️Income",
+		"arabic": "⬇️دخل",
+	}
+
+	Spendings = map[string]string{
+		"uz":     "⬆️Chiqim",
+		"ru":     "⬆️Расход",
+		"en":     "⬆️Spendings",
+		"arabic": "⬆️المصروفات",
+	}
+
+	Report = map[string]string{
+		"uz":     "📊Hisobot",
+		"ru":     "📊Отчет",
+		"en":     "📊Report",
+		"arabic": "📊تقرير",
+	}
+
+	Settings = map[string]string{
+		"uz":     "⚙️Sozlamalar",
+		"ru":     "⚙️Настройки",
+		"en":     "⚙️Settings",
+		"arabic": "⚙️الإعدادات",
+	}
+
+	// Report menu buttons
+	ReportForDay = map[string]string{
+		"uz":     "📈Kunlik hisobot",
+		"ru":     "📈Отчет за день",
+		"en":     "📈Report for day",
+		"arabic": "📈تقرير ليوم",
+	}
+
+	ReportForWeek = map[string]string{
+		"uz":     "📈Haftalik hisobot",
+		"ru":     "📈Отчет за неделю",
+		"en":     "📈Report for week",
+		"arabic": "📈تقرير لأسبوع",
+	}
+
+	ReportForMonth = map[string]string{
+		"uz":     "📈Oylik hisobot",
+		"ru":     "📈Отчет за месяц",
+		"en":     "📈Report for month",
+		"arabic": "📈تقرير لشهر",
+	}
+
+	ReportForYear = map[string]string{
+		"uz":     "📈Yillik hisobot",
+		"ru":     "📈Отчет за год",
+		"en":     "📈Report for year",
+		"arabic": "📈تقرير لعام",
+	}
+
+	// Settings menu buttons
+	Currencies = map[string]string{
+		"uz":     "💵Valyutalar",
+		"ru":     "💵Валюты",
+		"en":     "💵Currencies",
+		"arabic": "💵العملات",
+	}
+
+	Remainder = map[string]string{
+		"uz":     "🔔Eslatma",
+		"ru":     "🔔Напоминание",
+		"en":     "🔔Remainder",
+		"arabic": "🔔تذكير",
+	}
+
+	Language = map[string]string{
+		"uz":     "🌐Til",
+		"ru":     "🌐Язык",
+		"en":     "🌐Language",
+		"arabic": "🌐لغة",
+	}
+
+	// Currencies menu buttons
+	USD = map[string]string{
+		"uz":     "USD",
+		"ru":     "USD",
+		"en":     "USD",
+		"arabic": "USD",
+	}
+
+	EUR = map[string]string{
+		"uz":     "EUR",
+		"ru":     "EUR",
+		"en":     "EUR",
+		"arabic": "EUR",
+	}
+
+	RUB = map[string]string{
+		"uz":     "RUB",
+		"ru":     "RUB",
+		"en":     "RUB",
+		"arabic": "RUB",
+	}
+
+	UZS = map[string]string{
+		"uz":     "UZS",
+		"ru":     "UZS",
+		"en":     "UZS",
+		"arabic": "UZS",
+	}
+
+	// Remainder menu buttons
+	EveryHour = map[string]string{
+		"uz":     "Har soat",
+		"ru":     "Каждый час",
+		"en":     "Every hour",
+		"arabic": "كل ساعة",
+	}
+
+	EverySixHours = map[string]string{
+		"uz":     "Har 6 soatda",
+		"ru":     "Каждые 6 часов",
+		"en":     "Every 6 hours",
+		"arabic": "كل 6 ساعات",
+	}
+
+	EveryTwelveHours = map[string]string{
+		"uz":     "Har 12 soatda",
+		"ru":     "Каждые 12 часов",
+		"en":     "Every 12 hours",
+		"arabic": "كل 12 ساعة",
+	}
+
+	EveryDay = map[string]string{
+		"uz":     "Har kuni",
+		"ru":     "Каждый день",
+		"en":     "Every day",
+		"arabic": "كل يوم",
+	}
+)
